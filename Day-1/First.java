2f74856de517ea380ddbac63d942395f76b2e5de
eedekarunakar <95673054+eedekarunakar@users.noreply.github.com>
2024-11-06 16:32:41 +0530

    Add files via upload

---
+++ Day-1/First.java
@@ -0,0 +1,17 @@
+
+class First
+{
+   public static void main(String args[])
+   {
+
+      int a=10;
+      String name="Karun";
+      float ft=34.54F;
+ 
+     System.out.println(a);
+      System.out.println("The a value is:"+a);
+      System.out.println("The Given name is:"+name);
+      System.out.println("The Given Decimal value is:"+ft);
+    }
+}
+
